2af738df4bef0a08496f6b4b746f784a27456b4c
Faruk Emen <o.emen117@gmail.com>
2023-01-19 10:35:22 +0100

    test

---
+++ QuizApp/script.js
@@ -0,0 +1,11 @@
+// JSON Array //
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Tim Burnes Lee",
+        "answer_3": "Justin Bieber",
+        "answer_4": "Lady Gaga",
+        "right_asnwer": 2, 
+    },
+];
